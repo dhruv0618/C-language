700ed0f1e492154f667b13ab27dce405f1d3f43b
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-09-13 17:07:05 +0530

    initial comment

---
+++ loop.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main()
+{ //
+  /*int x, y;
+  printf("enter a two number");
+  scanf("%d %d", &x, &y);
+  x > 0 ? printf("positive") : printf("negative number");*/
+
+  /*  int x = 5, y = 10;
+    x++;
+    printf(" inter a two number");
+    scanf("%d %d", &x, &y);
+    ++x;
+    printf("greater number is");
+    printf(" %d and%d is %d\n", x, y, x + y);*/
+
+  //////////////////////////////loop///////////////////
+
+  /* int x = 1; // initialization
+  while (x <= 5) condition
+  {
+    printf("dhruv gupta");
+    ++x; flow  */
+
+  /* int x = 1;
+   do
+   {
+     printf("\ndhruv gupta");
+     x++;
+   } while (x <= 100); */
+
+  int i;
+  for (i = 1; i <= 5; i++)
+  {
+    printf("dhruv gupta");
+    i++;
+  }
+
+  /*int i = 1, x;
+  while (i <= 5)
+  {
+    printf("enter a number");
+    scanf("%d", &x);
+    if (x > 0)
+      break;
+    i++;
+  }
+  i == 6 ? printf("ends normaly") : printf("applied break");*/
+
+  ///////////////////////////////////switch case/////////////////////////
+}
