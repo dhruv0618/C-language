35e1ae39bded699e32a121d99d7cd1e26599a773
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-09-22 21:48:11 +0530

    second commit

--- Loop/ForLoop.c
+++ Loop/ForLoop.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int i;
-    for (i = 1; i <= 10; i++)
-    {
-        printf("%d ", i);
-    }
 
-    return 0;
+    /* int i;
+     for (i = 1; i <= 10; i++)
+     {
+         printf("%d ", i);
+     }
+     printf("End");
+
+     return 0; */
+
+    /*int i = 101;
+     for (; i <= 100; i++)
+     {
+         printf("%d ", i);
+     }
+     printf("End"); */
+
+    int i, j;
+    for (i = 1, j = 0; i = 5, j < 3; i++, j++)
+    {
+        printf("%d,%d ", i, j);
+    }
 }
