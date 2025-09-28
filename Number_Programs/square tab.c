93f2fc30a1f0817c6300c71a51c91f0fdb656ff1
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-09-28 12:28:56 +0530

    Square tab

---
+++ Number_Programs/square tab.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main;
+{
+
+    int number, i;
+    printf("Enter a number = %d ");
+    for (i = 1; i = 50; i++)
+    {
+        printf("%d is %d\n", i, i * i);
+    }
--- Number_Programs/square.c
+++ Number_Programs/square.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 int main()
 {
-
-    int x;
+    /*int x;
     printf("Enter a number ");
     scanf("%d", &x);
-    printf("Square of %d is %d\n", x, x * x);
+   */
+
+    // int  num,i;
+    // printf("Enter a number = %d");
+    for (int i = 1; i <= 10; i++)
+    {
+
+        printf("Square of %d is %d\n", i, i * i);
+    }
+    return 0;
 }
