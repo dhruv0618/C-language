3cb16268ec7ba49704b18fe4fecd74feb04d3c1e
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-10-05 19:43:10 +0530

    second

--- Functions/functiondemo.c
+++ Functions/functiondemo.c
@@ -9,9 +9,22 @@ void sum()
     sum = a + b;
     printf("sum = %d \n", sum);
 }
+void machar()
+{
+    printf("M machar hu ....\n");
+} 
+{
+    int subtract(int x, int y)
+    return x - y;
+}
 int main()
 {
-
+    machar();
     sum();
+    int a, b;
+    printf("Enter two number: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("Subtraction is : %d", subtract(a, b));
     return 0;
 }
