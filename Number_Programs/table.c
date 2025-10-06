e77db6a87314fdc59f3b5947aee0d34d82f47e5f
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-10-06 13:47:51 +0530

    table

---
+++ Number_Programs/table.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int number, i, a;
+    printf("Enter a number =  ");
+    scanf("%d", &number);
+    for (i = 1; i <= 10; i++)
+    {
+        a = i * number;
+        printf("%d\n", a);
+    }
+    return 0;
+}
