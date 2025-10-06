b0d2ff70afb9883f39820a93ad1ab363a39cc655
dhruvoffical.18@gmail.com <dhruvoffical.18@gmail.com>
2025-10-06 20:10:02 +0530

    while

---
+++ Loop/whileloop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1; // initialization
+    while (i <= 10)
+    {
+        printf("%d\n", i); // condition
+        i++;               // modification
+    }
+    printf("loop end");
+}
